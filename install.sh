27e62c1969c1b9ef1e23fbb2c6355f6e6bb4481b
nicholasadamou <nicholasadamouemail@gmail.com>
2017-11-18 08:16:51 -0500

    updated

--- install.sh
+++ install.sh
@@ -49,10 +49,43 @@ create_symlinks() {
     done
 }
 
+install_plugins() {
+
+    declare -r VUNDLE_DIR="$HOME/.vim/plugins/Vundle.vim"
+    declare -r VUNDLE_GIT_REPO_URL="https://github.com/VundleVim/Vundle.vim.git"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Install plugins.
+
+    execute \
+        "rm -rf '$VUNDLE_DIR' \
+            && git clone --quiet '$VUNDLE_GIT_REPO_URL' '$VUNDLE_DIR' \
+            && printf '\n' | vim +PluginInstall +qall" \
+        "Install plugins" \
+        || return 1
+
+}
+
+update_plugins() {
+
+    execute \
+        "vim +PluginUpdate +qall" \
+        "Update plugins"
+
+}
+
 main() {
     print_in_purple "\n • Create VIM symbolic links\n\n"
 
 	create_symlinks
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_in_purple "\n   Vim\n\n"
+
+    install_plugins
+    update_plugins
 }
 
 main
